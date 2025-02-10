aa97eac9be2b80fde299bf5a728795e1d44f7f6a
Victoria Nikolaeva <150319175+victoria-nikolaeva04@users.noreply.github.com>
2025-02-10 03:06:49 -0500

    Created debug log messages that are bad CISC jokes

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why did the women scream when she went into her office?");
+        Debug.Log("She saw a mouse on her desk!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
